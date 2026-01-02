6b66651618fce4bb85c3f682e6423d903b2a7216
AQI-sunny <2656099382@qq.com>
2026-01-03 03:56:01 +0800

    Add files via upload

---
+++ BOSSvanish/1231搜索数据备份.js
@@ -0,0 +1,103 @@
+ this.keywordsData = {
+            "version": "1.0",
+            "lastUpdated": "2024-01-01",
+            "searchRules": [
+                {
+                    "id": "medical_record",
+                    "keywords": ["5YW05YGl56eR5oqA6ZuG5Zui","5YW05YGl56eR5oqA6ZuG5Zui"],
+                    "targetUrl": "xjjt.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "jqtjl",
+                    "keywords": ["5a6J55u+56eR5oqA5YWs5Y+4","5a6J55u+56eR5oqA"],
+                    "targetUrl": "andun.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "jqtjl",
+                    "keywords": ["5p2O5YGl5YW0","x6eR5oqA"],
+                    "targetUrl": "shizong.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "jqtjl",
+                    "keywords": ["5YW055ub5Yi26YCg","5YW055ub5Yi26YCg"],
+                    "targetUrl": "baozha.html",
+                    "description": "该页面暂无描述..."
+                },
+               /*  {
+                    "id": "jqtjl",
+                    "keywords": ["6JKL5pm05aSp"],
+                    "targetUrl": "jqtjl.html",
+                    "description": "该页面暂无描述..."
+                }, */
+                {
+                    "id": "population_flow",
+                    "keywords": ["5L2V5a6J", "5byg5aiB", "5paH5b+D56u5"],
+                    "targetUrl": "snxyjz.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "population_flow",
+                    "keywords": ["5paH5b+D56u5 ", "5a6J5b+D6I2v5bqX", "5a6J5b+D6I2v5oi/"],
+                    "targetUrl": "anxin.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "slot_machine",
+                    "keywords": ["5Ym N5Y+w", "N+agi zQwMQ=="],
+                    "targetUrl": "qiantai.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "wd",
+                    "keywords": ["5pe66L6+5ZWG5ZyI", "5pe66L6+"],
+                    "targetUrl": "wangd.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "langren",
+                    "keywords": ["6J6C5Lq65rKZ", "5Luj5Y+36J6C5Lq65rKZ", "6J6C5Lq65rKZ5qGj5qGI"],
+                    "targetUrl": "langr.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "shehjg",
+                    "keywords": ["56S+5Lya57uT5p6E", "57uT5p6E56S+5Lya"],
+                    "targetUrl": "yjyjyjj.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "langrentu",
+                    "keywords": ["ZXZpZGVuY2VfcGhvdG8xLmh0bWw=", "ZXZpZGVuY2VfcGhvdG8x"],
+                    "targetUrl": "langren.jpg",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "chajianli",
+                    "keywords": ["5qGj5qGI566h55CG5a6k5peg6aKE57qm6YCa6YGT", "5qGj5qGI566h55CG5a6k5peg6aKE57qm"],
+                    "targetUrl": "chajlquanxianm.html",
+                    "description": "该页面暂无描述..."
+                },
+                {
+                    "id": "chajianli",
+                    "keywords": ["57mB5q6W6KGM5Li65oql5ZGK", "57mB5q6W6KGM5Li6"],
+                    "targetUrl": "fanzhi.html",
+                    "description": "该页面暂无描述..."
+                }, 
+                {
+                    "id": "chajianli",
+                    "keywords": ["Q1RIQw==", "Y3RoYw=="],
+                    "targetUrl": "todsk.html",
+                    "description": "该页面暂无描述..."
+                }, 
+                {
+                    "id": "asset_assessment",
+                    "keywords": ["5qmE5YyW5YmC", "6ZiI55WM5qmE5YyW5YmC"],
+                    "targetUrl": "chanpin.html",
+                    "description": "该页面暂无描述..."
+                }
+            ],
+            "recentPages": []
+        };
